e41c21246ad4225a271ef9f2aff22d5325464a18
codl <codl@codl.fr>
2016-10-11 15:41:45 +0200

    prompt: save prompt colour so it doesn't get recomputed on every new shell

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -6,7 +6,7 @@ function fish_prompt
     if not set -q __fish_prompt_colour
         set -l colours green cyan yellow blue magenta
         set -l host_hash (math ibase=16\;(hostname | md5sum | tr '[a-f]' '[A-F]' | head -c15))
-        set -g __fish_prompt_colour $colours[(math $host_hash%(count $colours)+1)]
+        set -U __fish_prompt_colour $colours[(math $host_hash%(count $colours)+1)]
     end
 
     echo -n -s (set_color $__fish_prompt_colour) (set_reverse) $__fish_prompt_shortened_hostname " " (prompt_pwd) (set_reverse) (set_color normal) (__prompt_trailing) " "
