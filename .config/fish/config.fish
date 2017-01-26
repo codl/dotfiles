fe8c6b6ddc05a0db5a0cf7fefe8d1f32fe26d330
codl <codl@codl.fr>
2017-01-26 15:36:20 +0100

    Revert "fish telemetry"
    This reverts commit 948240b32ecf07370155d9aaaad7f92e226fa335.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
 if status --is-login
     exec bash -l -c "source ~/.profile; exec fish -i"
 end
-
-if status --is-interactive
-    telemetry shell-opened
-end
--- .config/fish/functions/telemetry.fish
+++
@@ -1,3 +0,0 @@
-function telemetry -a dest
-    curl -s -XPOST -d "host="(hostname) https://beacon.codl.fr/self-telemetry/$dest &
-end
