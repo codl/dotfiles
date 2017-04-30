1823faea0221c67592b514675f766434f7bd0d6a
codl <codl@codl.fr>
2017-04-30 02:29:56 +0200

    fix fish hang when offline

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     if not set -q __fish_prompt_shortened_hostname
-        set -g __fish_prompt_shortened_hostname (hostname -s|head -c 4)
+        set -g __fish_prompt_shortened_hostname (hostname | cut -f 1 -d . |head -c 4)
     end
 
     echo -n -s (set_color (__prompt_color)) (set_reverse) $__fish_prompt_shortened_hostname " " (prompt_pwd) (set_reverse) (set_color normal) (__prompt_trailing) " "
