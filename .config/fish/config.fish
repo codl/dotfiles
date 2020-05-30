ef4e243713053dd0bf377f1cdffa479f3ab968e7
codl <codl@codl.fr>
2020-05-30 03:55:05 +0200

    config.fish: fix ametameric not installing if rvm isn't present

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,11 @@ if status --is-login
     exec bash -l -c "source ~/.profile; exec fish -i"
 end
 
-if status --is-interactive; and test -d $HOME/.rbenv
-    set -x PATH $HOME/.rbenv/bin $PATH
-    source (rbenv init -|psub)
+if status --is-interactive
     ametameric -r
+
+    if test -d $HOME/.rbenv
+        set -x PATH $HOME/.rbenv/bin $PATH
+        source (rbenv init -|psub)
+    end
 end
