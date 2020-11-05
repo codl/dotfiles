66e016a6a3913c3bd1c9c849f8f12b37c899dd58
codl <codl@codl.fr>
2020-11-05 11:59:04 +0100

    fish oggify: raise vbr quality setting

--- .config/fish/functions/oggify.fish
+++ .config/fish/functions/oggify.fish
@@ -1,7 +1,7 @@
 function oggify
     find -name '*.flac' | while read f
         echo "$f"
-        ffmpeg -i "$f" -hide_banner -v error -nostdin -map 0:a -c:a libvorbis -y "$f.ogg"
+        ffmpeg -i "$f" -hide_banner -v error -nostdin -map 0:a -c:a libvorbis -q 5 -y "$f.ogg"
         and metaflac --export-picture-to=/tmp/oggify-pic.tmp "$f"
         and kid3-cli -c 'set picture:"/tmp/oggify-pic.tmp" "front"' "$f.ogg"
         and rm "$f"
