e9518b1d1c3307158e9937bbd9cd6e0f126ee15b
codl <codl@codl.fr>
2023-05-17 00:16:26 +0200

    ametameric.fish: update branch name in user agent

--- .config/fish/functions/ametameric.fish
+++ .config/fish/functions/ametameric.fish
@@ -36,7 +36,7 @@ function ametameric
         mkdir -p $CACHEDIR
         echo "Downloading $filename in the background..."
         wget -q "https://pippin.gimp.org/ametameric/"$filename \
-            --user-agent="ametameric.fish +https://github.com/codl/dotfiles/blob/master/.config/fish/functions/ametameric.fish" \
+            --user-agent="ametameric.fish +https://github.com/codl/dotfiles/blob/main/.config/fish/functions/ametameric.fish" \
             -O $CACHEDIR/$filename &
         function _postdownload --inherit-variable filename --on-process-exit (jobs --last --pid)
             __ametameric_init $filename
