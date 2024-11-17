8c612e22d1a20223e7cb7ec77ce752c0744a327d
codl <codl@codl.fr>
2024-11-17 11:07:37 +0100

    get-uv: ensure ~/.local/bin exists

--- .config/fish/functions/get-uv.fish
+++ .config/fish/functions/get-uv.fish
@@ -1,4 +1,5 @@
 function get-uv
+    mkdir -p ~/.local/bin
     pushd ~/.local/bin
     and curl -L https://github.com/astral-sh/uv/releases/download/$argv[1]/uv-x86_64-unknown-linux-gnu.tar.gz | gunzip | tar xv --strip-components=1
     popd
